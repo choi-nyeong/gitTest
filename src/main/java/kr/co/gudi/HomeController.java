559c97e6a56e75bca3a6510a9b9a887bf11c65d0
choieunyoung <coo810@naver.com>
2023-04-27 09:47:21 +0900

    최초 버전 작성

---
+++ src/main/java/kr/co/gudi/HomeController.java
@@ -0,0 +1,25 @@
+package kr.co.gudi;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		model.addAttribute("msg","hello git");
+		
+		return "home";
+	}
+	
+}
